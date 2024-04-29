ac343521b135edc7bc7995ca7a3fafc2d44860c1
SKRINTRAP <163513483+Baastan@users.noreply.github.com>
2024-04-29 22:14:20 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let color1 = ()=>{
+    document.body.style.backgroundColor = 'wheat'
+} 
+let color2 = ()=>{
+    document.body.style.backgroundColor = 'gray'
+} 
+let color3 = ()=>{
+    document.body.style.backgroundColor = 'dimgray'
+} 
+
